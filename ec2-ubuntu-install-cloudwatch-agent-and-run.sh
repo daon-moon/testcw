a2851de98c2bbc2c4b298b3aaffce2ed06fa0abb
daon-moon <63081044+daon-moon@users.noreply.github.com>
2020-06-17 08:53:14 +0900

    Update ec2-ubuntu-install-cloudwatch-agent-and-run.sh

--- ec2-ubuntu-install-cloudwatch-agent-and-run.sh
+++ ec2-ubuntu-install-cloudwatch-agent-and-run.sh
@@ -9,19 +9,14 @@ CWFILE=/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl
 if test -f "$CWFILE"; then
     echo "$CWFILE exist"
 else
-   # wget  https://s3.ap-northeast-2.amazonaws.com/amazoncloudwatch-agent-ap-northeast-2/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-    wget https://s3.ap-northeast-2.amazonaws.com/amazoncloudwatch-agent-ap-northeast-2/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-
-
-   # sudo rpm -U ./amazon-cloudwatch-agent.rpm
-    sudo dpkg -i -E ./amazon-cloudwatch-agent.deb	
-
+   wget https://s3.ap-northeast-2.amazonaws.com/amazoncloudwatch-agent-ap-northeast-2/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+   sudo dpkg -i -E ./amazon-cloudwatch-agent.deb	
 fi
 
-aws s3 cp s3://hmx-cloudop-fileshare-01/cloudwatch-agent-config/amazon-cloudwatch-agent-mem-linux.json /home/ubuntu/
+#aws s3 cp s3://hmx-cloudop-fileshare-01/cloudwatch-agent-config/amazon-cloudwatch-agent-mem-linux.json /home/ubuntu/
 
-sudo cp  amazon-cloudwatch-agent-mem-linux.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo cp /home/ubuntu/amazon-cloudwatch-agent-mem-linux.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
-sudo  /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
