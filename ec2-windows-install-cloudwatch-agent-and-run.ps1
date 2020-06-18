e4b64e49558ded670f6c518c62211fe9da82eb04
daon-moon <63081044+daon-moon@users.noreply.github.com>
2020-06-18 10:41:12 +0900

    Update ec2-windows-install-cloudwatch-agent-and-run.ps1

--- ec2-windows-install-cloudwatch-agent-and-run.ps1
+++ ec2-windows-install-cloudwatch-agent-and-run.ps1
@@ -1,8 +1,13 @@
 cd C:\Users\Administrator
 
+CWFILE=C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1
+
+if test -f "$CWFILE"; then
+  ehco "$CWFILE exist"
+  else
 wget https://s3.ap-northeast-2.amazonaws.com/amazoncloudwatch-agent-ap-northeast-2/windows/amd64/latest/amazon-cloudwatch-agent.msi -OutFile ./amazon-cloudwatch-agent.msi
 msiexec /i amazon-cloudwatch-agent.msi
-
+  fi
 wget https://raw.githubusercontent.com/daon-moon/testcw/master/amazon-cloudwatch-agent-mem-windows.json -OutFile ./amazon-cloudwatch-agent-mem-windows.json
 
 & Copy-Item -Path "C:\Users\Administrator\amazon-cloudwatch-agent-mem-windows.json" -Destination "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-mem-windows.json"
