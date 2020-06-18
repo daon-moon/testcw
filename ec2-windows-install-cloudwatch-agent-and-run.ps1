29f6ddb474fe80177f7e846db09a5e73332870f5
daon-moon <63081044+daon-moon@users.noreply.github.com>
2020-06-18 10:48:05 +0900

    Update ec2-windows-install-cloudwatch-agent-and-run.ps1

--- ec2-windows-install-cloudwatch-agent-and-run.ps1
+++ ec2-windows-install-cloudwatch-agent-and-run.ps1
@@ -1,9 +1,11 @@
 cd C:\Users\Administrator
 
-CWFILE=C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1
+$CWFILE=C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1
 
-if test -f "$CWFILE"; then
-  ehco "$CWFILE exist"
+if (Test-Path $CWFILE -PathType leaf)
+  {
+  "Feile does exist"
+  }
   else
 wget https://s3.ap-northeast-2.amazonaws.com/amazoncloudwatch-agent-ap-northeast-2/windows/amd64/latest/amazon-cloudwatch-agent.msi -OutFile ./amazon-cloudwatch-agent.msi
 msiexec /i amazon-cloudwatch-agent.msi
