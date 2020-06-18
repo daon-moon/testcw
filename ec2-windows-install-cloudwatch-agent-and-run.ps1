4b3a45e6cb3ca7d6af860b595b17e07ddef0dfc7
daon-moon <63081044+daon-moon@users.noreply.github.com>
2020-06-18 09:19:01 +0900

    Update ec2-windows-install-cloudwatch-agent-and-run.ps1

--- ec2-windows-install-cloudwatch-agent-and-run.ps1
+++ ec2-windows-install-cloudwatch-agent-and-run.ps1
@@ -1,5 +1,3 @@
-<powershell>
-
 cd C:\Users\Administrator
 
 wget https://s3.ap-northeast-2.amazonaws.com/amazoncloudwatch-agent-ap-northeast-2/windows/amd64/latest/amazon-cloudwatch-agent.msi -OutFile ./amazon-cloudwatch-agent.msi
@@ -13,8 +11,6 @@ cp -Path "C:\Users\Administrator\amazon-cloudwatch-agent-mem-windows.json" -Dest
 & "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1" -m ec2 -a stop
 
 & "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1" -a fetch-config -m ec2 -c "file:C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-mem-windows.json" -s
-
-</powershell>
 
 
 
