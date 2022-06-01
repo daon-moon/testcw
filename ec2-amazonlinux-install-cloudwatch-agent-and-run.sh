6cd07ae75a890ea5796938f9eb5e75e8227a938b
daon-moon <63081044+daon-moon@users.noreply.github.com>
2022-06-01 18:51:15 +0900

    Update ec2-amazonlinux-install-cloudwatch-agent-and-run.sh

--- ec2-amazonlinux-install-cloudwatch-agent-and-run.sh
+++ ec2-amazonlinux-install-cloudwatch-agent-and-run.sh
@@ -17,7 +17,7 @@ fi
 wget https://hmx-cloudop-share-ext.s3.ap-northeast-2.amazonaws.com/aws-cw-json/hel-sap/hel-linux-com.json
 #aws s3 cp s3://hmx-cloudop-fileshare-01/cloudwatch-agent-config/amazon-cloudwatch-agent-mem-linux.json /home/ec2-user/
 
-sleep 5
+sleep 7
 
 sudo cp /home/ec2-user/hel-linux-com.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent-linux.json
 
