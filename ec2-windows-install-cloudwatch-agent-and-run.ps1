4a966c3bb7c03589fe1024ab06edd27b832b58e5
daon-moon <63081044+daon-moon@users.noreply.github.com>
2022-06-02 12:25:47 +0900

    Update ec2-windows-install-cloudwatch-agent-and-run.ps1

--- ec2-windows-install-cloudwatch-agent-and-run.ps1
+++ ec2-windows-install-cloudwatch-agent-and-run.ps1
@@ -9,8 +9,8 @@ if (Test-Path $CWFILE -PathType leaf)
 
 else
   {
- #& wget https://raw.githubusercontent.com/daon-moon/testcw/master/amazon-cloudwatch-agent-windows.json -OutFile ./amazon-cloudwatch-agent-windows.json
- & wget https://hmx-cloudop-share-ext.s3.ap-northeast-2.amazonaws.com/aws-cw-json/hel-sap/hel-windows-com.json -OutFile ./amazon-cloudwatch-agent-windows.json
+ & wget https://raw.githubusercontent.com/daon-moon/testcw/master/amazon-cloudwatch-agent-windows.json -OutFile ./amazon-cloudwatch-agent-windows.json
+ #& wget https://hmx-cloudop-share-ext.s3.ap-northeast-2.amazonaws.com/aws-cw-json/hel-sap/hel-windows-com.json -OutFile ./amazon-cloudwatch-agent-windows.json
 
  & wget https://s3.ap-northeast-2.amazonaws.com/amazoncloudwatch-agent-ap-northeast-2/windows/amd64/latest/amazon-cloudwatch-agent.msi -OutFile ./amazon-cloudwatch-agent.msi   
      
@@ -25,11 +25,9 @@ else
 if(Test-Path $CWFILE -PathType leaf){
   & Copy-Item -Path "C:\Users\Administrator\amazon-cloudwatch-agent-windows.json" -Destination "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-windows.json" -Force
   
-  #& Copy-Item -Path "C:\Users\Administrator\amazon-cloudwatch-agent-windows.json" -Destination "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-windows.json" -Force
-  
   & "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1" -m ec2 -a stop
   
-  & "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1" -a fetch-config -m ec2 -c "file:C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-windows.json" -s    
+  & "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1" -a fetch-config -m ec2 -c "file:C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-windows.json" -s
 }
 else{
  "Check again"
